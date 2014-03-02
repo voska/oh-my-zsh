7421b1b2971c158b0c4c5993f06bc81739c16fa2
Aakash Patel <aakpat6@gmail.com>
2014-03-02 16:38:31 -0500

    Fix terminal title.

--- lib/termsupport.zsh
+++ lib/termsupport.zsh
@@ -14,8 +14,8 @@ function title {
   fi
 }
 
-ZSH_THEME_TERM_TAB_TITLE_IDLE="%15<..<%~%<<" #15 char left truncated PWD
-ZSH_THEME_TERM_TITLE_IDLE="%n@%m: %~"
+ZSH_THEME_TERM_TAB_TITLE_IDLE="zsh" #15 char left truncated PWD
+ZSH_THEME_TERM_TITLE_IDLE="zsh"
 
 #Appears when you have the prompt
 function omz_termsupport_precmd {
