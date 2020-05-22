98b517389a82364a3e5c4a97118eb56f86271e3a
Matt Voska <mattvoska@gmail.com>
2020-05-22 10:24:15 -0400

    Upgrade brewski command to update all casks.

--- custom/voska.zsh
+++ custom/voska.zsh
@@ -1,4 +1,4 @@
 # Aliases
 alias show='defaults write com.apple.finder AppleShowAllFiles TRUE && killall Finder'
 alias hide='defaults write com.apple.finder AppleShowAllFiles FALSE && killall Finder'
-alias brewski='brew update && brew upgrade && brew cask upgrade && brew cleanup -s && brew doctor; brew missing'
+alias brewski='brew update && brew upgrade && brew cask upgrade --greedy && brew cleanup -s && brew doctor; brew missing'
