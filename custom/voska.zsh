6e1951ba22676c974d20397ef5f5e695e55ae54a
Matt Voska <mattvoska@gmail.com>
2020-09-21 15:52:10 +0200

    Updates brewski.

--- custom/voska.zsh
+++ custom/voska.zsh
@@ -1,5 +1,5 @@
 # Aliases
 alias show='defaults write com.apple.finder AppleShowAllFiles TRUE && killall Finder'
 alias hide='defaults write com.apple.finder AppleShowAllFiles FALSE && killall Finder'
-alias brewski='brew update && brew upgrade && brew cask upgrade --greedy && brew cleanup -s && brew doctor; brew missing'
+alias brewski='brew update && brew upgrade --greedy && brew cleanup -s && brew doctor; brew missing'
 alias git-pull-all='find . -maxdepth 3 -name .git -type d | rev | cut -c 6- | rev | xargs -I {} git -C {} pull'
